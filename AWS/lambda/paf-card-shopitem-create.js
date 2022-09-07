cbc9adbaec7b171dd13c2d5d2181b15eac41604f
mithsin <hsinbluemoon@gmail.com>
2022-09-07 11:36:45 -0400

    add lambda function

---
+++ AWS/lambda/paf-card-shopitem-create.js
@@ -0,0 +1,24 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = (event, context, callback) => {
+    const params = {
+        Item: {
+            "itemId": `shopItem-${context.awsRequestId}`,
+            "itemDescription": event.itemDescription,
+            "itemName": event.itemName,
+            "points": event.points,
+            "image": event.image
+        },
+        TableName: "paf-card"
+    };
+    dynamodb.put(params, function(err, data){
+        if(err){
+            console.log(err);
+            callback(err);
+        } else {
+            console.log(data);
+            callback(null, { status: 200, upload_status: 'success' });
+        }
+    });
+};
